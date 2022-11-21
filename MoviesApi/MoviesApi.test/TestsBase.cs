0b3c09e10c5825dc9b6beb6bfe65c78fd7b09e58
Diego Dacak <diego.dacak@rugertek.com>
2022-11-20 21:07:05 -0300

    change in test base

--- MoviesApi/MoviesApi.test/TestsBase.cs
+++ MoviesApi/MoviesApi.test/TestsBase.cs
@@ -1,6 +1,4 @@
-﻿
-
-using AutoMapper;
+﻿using AutoMapper;
 using Microsoft.EntityFrameworkCore;
 using MoviesApi.Context;
 using MoviesApi.Services.AutoMapper;
@@ -10,7 +8,7 @@ namespace MoviesApi.test;
 
 public class TestsBase
 {
-    protected ApplicationDbContext BuildContext(string dbName)
+    protected static ApplicationDbContext BuildContext(string dbName)
     {
         var options = new DbContextOptionsBuilder<ApplicationDbContext>()
             .UseInMemoryDatabase(dbName).Options;
